f4872787fdd8d857b834419c09502d1e0038d8e9
ljpson <ljpson@learnershi.com>
2023-03-24 16:46:43 +0900

    회원가입 시작

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 5000
+const bodyParser = require('body-parser')
+const { User } = require('./model/User')
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
+
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://ljpson:ydmbe5HclPpikpeb@studycluster.fpihgtz.mongodb.net/?retryWrites=true&w=majority')
-.then(() => console.log('MongoDB Connected...'))
-.catch(err => console.log(err))
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', (req, res) => { res.send('Hello World!') })
+
+app.post('/register', (req, res) => {
+
+  // 회원가입할 때 필요한 정보들을 Client에서 가져오면
+
+  // 그것들을 데이터베이스에 넣어준다
+
+  const user = new User(req.body)
+
+  user.save((err, doc) => {
+    if(err) return res.status(400).json({success: false, err})
+    return res.status(200).json({success: true})
+  })
+
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+
+app.listen(port, () => { console.log(`Example app listening on port ${port}`) })
